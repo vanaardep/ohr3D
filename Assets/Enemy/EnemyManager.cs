51e3fe9d3a3ddecfc8567e9c3e3bec7ac5613401
Juan-Paul Naude <ju.naude@gmail.com>
2015-07-29 21:15:16 +0200

    Base Game
    Fix moving the character forwards and backwards,attach enemyMove to
    ghoul prefab to get them to move etc.

---
+++ Assets/Enemy/EnemyManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections;
+
+public class EnemyManager : MonoBehaviour {
+
+	public GameObject enemy;
+	public float spawntime = 3f;
+	public float startTime = 1f;
+	public Transform[] spawnPoints;
+	int numberOfEnemies = 1;
+	
+	void Start () {
+		//This function calls the spawn method repeatedly every 3 seconds
+		InvokeRepeating ("spawn", startTime, spawntime);
+	}
+
+	/**
+	 * function spawn() creates an enemy at one of the spawn points
+	 * */
+	void spawn()
+	{
+		 //will stop the spawning function once player or base health reaches 0
+		/*if (PlayerHealth.playerHealth <= 0 || BaseCarHealth.baseCarHealth <=0) 
+		{
+			return;
+		}*/
+		int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+		Instantiate (enemy, spawnPoints [spawnPointIndex].position, spawnPoints [spawnPointIndex].rotation);
+		numberOfEnemies++;
+
+		if (numberOfEnemies > 20) {
+			//CancelInvoke ("spawn");
+			return;
+		}
+		//Debug.Log ("Spawned enemy");
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ Assets/Enemy/EnemyMove.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMove : MonoBehaviour {
+
+	public AudioClip hurtSound2;
+
+	public Transform player;
+	Transform TeslaSpotlight;
+	public Transform baseCar;
+	
+	float speed = 1f;
+	//float angleSpread = 20;
+	//float enemyFreezeTime = 5;
+
+	// Distances and light	
+	bool frozen = false;
+	bool playingSound = false;
+	float distanceToPlayer;
+	float distanceToCar;
+	Vector3 playerZRotation;
+	Vector3 enemyPosition;
+	Vector3 facing;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player").transform;
+		frozen = false;
+		//TeslaSpotlight = GameObject.Find ("TeslaSpotlight").transform;
+		baseCar = GameObject.Find ("baseCar").transform;
+
+		InvokeRepeating ("checkDistances", 0, 1.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		/*if (distanceToPlayer <= 25 && ((facing.z - angleSpread) < playerZRotation.z && playerZRotation.z < (facing.z + angleSpread))) {
+			frozen = true;
+			
+			// Are we killing this guy?
+			//PlayerTeslaGlove gloveScript = TeslaSpotlight.GetComponent<PlayerTeslaGlove> ();
+			//Debug.Log (gloveScript.gloveActive);
+			
+			if (gloveScript.gloveActive) {
+				// Kill enemy
+				Debug.Log ("Finish Him!!");
+				if (!playingSound) {
+					//playingSound = true;
+					SoundManager.instance.PlaySingle(hurtSound2);
+				}
+
+				Destroy (this.gameObject);
+			}
+		}*/
+
+		//if (!frozen) {
+			// Check if enemy in angle
+			//========================
+			/*if (distanceToPlayer <= 25 && ((facing.z - angleSpread) < playerZRotation.z && playerZRotation.z < (facing.z + angleSpread))) {
+				frozen = true;
+
+				// Just freeze him
+				Debug.Log ("FREEEEZE BITCH");
+				Invoke ("unfreezeEnemy", enemyFreezeTime);
+			} */
+			
+			if (distanceToPlayer < distanceToCar) {
+				// Move towards player
+
+				transform.LookAt(player.position, Vector3.up);
+				transform.position = Vector3.MoveTowards (transform.position, player.position, speed * Time.deltaTime);
+			} else {
+				// Move towards car
+				transform.LookAt(baseCar.position, Vector3.up);
+				transform.position = Vector3.MoveTowards (transform.position, baseCar.position, speed * Time.deltaTime);
+			}
+		//} 
+	}
+
+	void checkDistances(){
+		// Distance from player
+		distanceToPlayer = (this.transform.position - player.position).sqrMagnitude;//Vector2.Distance (this.transform.position, player.transform.position);
+		distanceToCar = (this.transform.position - baseCar.position).sqrMagnitude;//Vector2.Distance (this.transform.position, baseCar.transform.position);
+		
+		// Player z
+		playerZRotation = player.rotation.eulerAngles;
+		
+		// Angle to player
+		//================
+		//Vector3 directionToTarget = player.position - this.transform.position;
+		//float angle = Vector3.Angle (player.forward, directionToTarget);
+		
+		//Vector3 targetDir = player.position - this.transform.position;
+		//Vector3 forward = this.transform.forward;
+		//float angle = Vector3.Angle(targetDir, forward);
+		
+		/*Vector2 heading = this.transform.position - player.position;
+		float distance2 = heading.magnitude;
+		Vector2 angle = heading / distance2; // This is now the normalized direction.*/
+		
+		//float angle = SignedAngleBetween(this.transform.position, player.position, player.position);
+		
+		//Grab the current mouse position on the screen
+		enemyPosition = this.transform.position;//camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y, Input.mousePosition.z - camera.transform.position.z));
+		
+		//Rotates toward the mouse
+		facing = new Vector3(0,0,Mathf.Atan2((enemyPosition.y - player.position.y), (enemyPosition.x - player.position.x))*Mathf.Rad2Deg + 270);
+		
+		//Debug.Log (facing.z + " VS " + playerZRotation.z);
+	}
+	
+	void unfreezeEnemy () {
+		frozen = false;
+	}
+}
