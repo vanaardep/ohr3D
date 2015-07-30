8bd9e45e848af13d268b0eb13311b28ebb41f5d7
Juan-Paul Naude <ju.naude@gmail.com>
2015-07-30 09:23:00 +0200

    Fixed the camera

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public float distance;
+	public float zDistance;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(target.position.x, target.position.y  + distance, target.position.z- - zDistance);
+	
+	}
+}
