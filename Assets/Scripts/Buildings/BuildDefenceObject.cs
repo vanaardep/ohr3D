ac682e07ddbf869a201587ec196bd8bcbcde85ca
vanaardep <pdvanaarde@gmail.com>
2015-08-03 18:15:41 +0200

    Added pylon and mine drop
    Use Q or E

---
+++ Assets/Scripts/Buildings/BuildDefenceObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildDefenceObject : MonoBehaviour {
+
+	//public Transform pylon;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyUp (KeyCode.E)) { //Deploy light pylon
+			Debug.Log(">>>> Place pylon");
+
+			Vector3 playerPosition = GameObject.Find("Auron").transform.position;
+
+			GameObject thisObject = Instantiate(Resources.Load("pylon"), playerPosition, Quaternion.identity) as GameObject;
+		}
+		if (Input.GetKeyUp (KeyCode.Q)) { //Deploy light pylon
+			Debug.Log(">>>> Place mine");
+			
+			Vector3 playerPosition = GameObject.Find("Auron").transform.position;
+			
+			GameObject thisObject = Instantiate(Resources.Load("lightMine"), playerPosition, Quaternion.identity) as GameObject;
+		}
+	}
+}
