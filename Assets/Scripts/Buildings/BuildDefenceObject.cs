edc6f50a3da95c3235ff127a6d115fa7a3e9f33c
vanaardep <pdvanaarde@gmail.com>
2015-08-17 21:21:06 +0200

    Fixed turret building issue..again

--- Assets/Scripts/Buildings/BuildDefenceObject.cs
+++ Assets/Scripts/Buildings/BuildDefenceObject.cs
@@ -4,16 +4,14 @@ using System.Collections;
 public class BuildDefenceObject : MonoBehaviour {
 
 	//public Transform pylon;
-	public AudioClip buildSound;
+	//public AudioClip buildSound;
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
 
+	// Update is called once per frame
+	void LateUpdate () {
 		if (Input.GetKeyDown (KeyCode.E)) { //Deploy light pylon
 			
 			Debug.Log(">>>> Place pylon");
@@ -22,7 +20,7 @@ public class BuildDefenceObject : MonoBehaviour {
 
 			GameObject thisObject = Instantiate(Resources.Load("pylon"), playerPosition, Quaternion.identity) as GameObject;
 			thisObject.transform.Translate(transform.forward * 1);
-			SoundManager.instance.PlaySingle(buildSound);
+			//SoundManager.instance.PlaySingle(buildSound);
 		}
 		if (Input.GetKeyDown (KeyCode.Q)) { //Deploy light pylon
 			
@@ -32,7 +30,7 @@ public class BuildDefenceObject : MonoBehaviour {
 			
 			GameObject thisObject = Instantiate(Resources.Load("lightMine"), playerPosition, Quaternion.identity) as GameObject;
 			thisObject.transform.Translate(transform.forward * 1);
-			SoundManager.instance.PlaySingle(buildSound);
+			//SoundManager.instance.PlaySingle(buildSound);
 		}
 
 		if (Input.GetKeyDown (KeyCode.R)) { //Deploy light turret
@@ -43,7 +41,7 @@ public class BuildDefenceObject : MonoBehaviour {
 			
 			GameObject thisObject = Instantiate(Resources.Load("turretPrefab"), playerPosition, Quaternion.identity) as GameObject;
 			thisObject.transform.Translate(transform.forward * 1);
-			SoundManager.instance.PlaySingle(buildSound);
+			//SoundManager.instance.PlaySingle(buildSound);
 		}
 	}
 }
