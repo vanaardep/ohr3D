d580e94ebef977f48767115ad409e5f3cd784c58
Daniel R Pearson <daniel.pearson@telkomsa.net>
2015-08-17 19:43:24 +0200

    Finished testing light turret

---
+++ Assets/Scripts/Buildings/LightTurretAttack.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightTurretAttack : MonoBehaviour {
+
+ 	private float fireStart = 0f;
+ 	private float fireCooldown = 0.5f; // 0.5 seconds of active
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+
+		if(Time.time > fireStart + fireCooldown) {
+			this.GetComponentInChildren<Light> ().intensity = 0;
+		}
+	}
+
+	void OnCollisionStay (Collision col) {
+        if((col.gameObject.tag == "Enemy")) {
+     			fireStart = Time.time;
+	   			Debug.Log("LIGHTTURRET ATTACKKKKKKKKKKKKKKKK");
+	            Destroy(col.gameObject);
+	    }
+        
+        else {
+         	this.GetComponentInChildren<Light> ().intensity = 8;
+        }
+         
+    }
+		
+}
