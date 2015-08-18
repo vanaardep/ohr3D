0024ba91cd3e92834f1431735993bdd5f7b89c07
Dirkie Boshoff <diedirq@gmail.com>
2015-08-18 02:53:16 +0200

    Glow sticks added. Light changes intensity when enemies close by
    Press G for glow stick

---
+++ Assets/Scripts/Buildings/glowStick.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class glowStick : MonoBehaviour {
+
+	public Light light;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void OnTriggerEnter(Collider other){
+
+		Debug.Log (other.gameObject.tag);
+		Debug.Log (other);
+		if (other.tag == "Enemy") { //If enemy enter collision radius turn red + increase intensity
+
+			light.intensity = 8;
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.name == "enemyCollider") { //When enemy leave, and none are left in radius turn back to green
+			light.intensity = 2;
+
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
