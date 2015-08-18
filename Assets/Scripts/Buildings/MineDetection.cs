0fd46cdc0c10f728433c4915102df91e801cef88
Dirkie Boshoff <diedirq@gmail.com>
2015-08-18 21:33:03 +0200

    Added particle system for light mines explosion

--- Assets/Scripts/Buildings/MineDetection.cs
+++ Assets/Scripts/Buildings/MineDetection.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class MineDetection : MonoBehaviour {
 
+
+
+
+
+
+
+	public ParticleSystem ps;
+
+
+	private Vector3 minePosition;
 	private bool timeToExplode = false;
 	public float mineExplosionDelay;
 	private bool timerActivated = false;
-
+	private float resetValue; //If script needs to be reset
+	private ParticleSystem tempPs; // temporaryParticleSystem
 	// Use this for initialization
 	void Start () {
-		
+		resetValue = mineExplosionDelay;
+		 minePosition = gameObject.transform.position ;
 	}
 	
 	// Update is called once per frame
@@ -25,17 +37,33 @@ public class MineDetection : MonoBehaviour {
 	}
 	
 	void OnTriggerEnter(Collider other) {
-		timerActivated = true;
-		
+		if (other.tag == "Enemy") {
+			timerActivated = true;
+			tempPs = Instantiate(ps, new Vector3(minePosition.x, minePosition.y +1, minePosition.z), Quaternion.identity) as ParticleSystem;
+			tempPs.Play();
+			Destroy(tempPs,tempPs.duration);
+			}
 	}
 	
 	void OnTriggerStay(Collider other) {
 		//Debug.Log (other.tag);
 		if (timeToExplode == true && other.tag == "Enemy") {
 			Destroy(other.gameObject);
-			PlayerGUI.batteryPerc+=5; // Everytime a mine kills an enemy gets +5 battery
 			Destroy(gameObject);
+			PlayerGUI.batteryPerc+=5; // Everytime a mine kills an enemy gets +5 battery
+
 		}
 		
 	}
+
+	void OnTriggerExit(Collider other){ 
+		if (other.tag == "Enemy") {
+			//Destroy(tempPs);
+			mineExplosionDelay = resetValue;
+			timeToExplode = false;
+			timerActivated = false;
+		}
+	}
+
+
 }
