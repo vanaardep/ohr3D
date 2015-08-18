c3c3ed70b63bb041a950c87d30c89eef4fb57ac1
Daniel R Pearson <daniel.pearson@telkomsa.net>
2015-08-18 10:15:13 +0200

    Light Turret functionality added.
    CD of 1 attack is 5seconds +/-
    light pulse animation lasts 2seconds +/-
    Destroys 1 Enemy at a time

---
+++ Assets/Scripts/Buildings/LightTurretAttack.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightTurretAttack : MonoBehaviour {
+
+ 	private float fireStart = 0f;
+ 	private float fireCooldown = 5f; // 5cd of turret
+
+ 	private float fireAnimStart = 0f;
+ 	private float fireAnimCooldown = 1f; // 1 second light pulse animation
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+
+		if(Time.time > fireAnimStart + fireAnimCooldown) {
+        		fireAnimStart = Time.time;
+         		this.GetComponentInChildren<Light> ().intensity = 0;
+         }
+	}
+
+	void OnCollisionStay (Collision col) {
+        if((col.gameObject.tag == "Enemy") && (Time.time > fireStart + fireCooldown)) {
+     			fireStart = Time.time;
+	   			Debug.Log("LIGHTTURRET ATTACKKKKKKKKKKKKKKKK");
+	            Destroy(col.gameObject);
+	            this.GetComponentInChildren<Light> ().intensity = 8;
+	    }
+         
+    }
+		
+}
