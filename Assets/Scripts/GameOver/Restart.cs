0ad7e8d410c07311275f7012410a7132ca79dd72
Juan-Paul Naude <ju.naude@gmail.com>
2015-08-24 10:15:05 +0200

    Small Changes

--- Assets/Scripts/GameOver/Restart.cs
+++ Assets/Scripts/GameOver/Restart.cs
@@ -1,9 +1,22 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Restart : MonoBehaviour {
+	public float timer = 0f;
 
-
+    void start()
+	{
+		timer = 0;
+	}
+    void update()
+	{
+		Debug.Log ("update");
+		timer += Time.deltaTime;
+		if (timer > 10) {
+			Debug.Log("In");
+			Application.LoadLevel ("Level1");
+		}
+	}
 	
 	// Update is called once per frame
 	public void ChangeToScene (string scenceToChange) {
