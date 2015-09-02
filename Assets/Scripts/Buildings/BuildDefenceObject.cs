dc6e9c6e39065bdf1e19afd0693d6a8b159fa8ba
Daniel R Pearson <daniel.pearson@telkomsa.net>
2015-09-02 15:04:30 +0200

    Construction Time added with sound
    Construction time is 5 seconds.
    Still need to implement construction visuals

--- Assets/Scripts/Buildings/BuildDefenceObject.cs
+++ Assets/Scripts/Buildings/BuildDefenceObject.cs
@@ -4,28 +4,63 @@ using System.Collections;
 public class BuildDefenceObject : MonoBehaviour {
 
 	public static int signalCount;
+	private int constructionTime;
 	public AudioClip buildSound;
+	public AudioClip constructionTimeSoundBegin;
+	public AudioClip constructionTimeSoundEnd;
 	// Use this for initialization
 	void Start () {
 		signalCount = 4;
+		constructionTime = 5;
 	}
 
-	// Update is called once per frame
-	void LateUpdate () {
-		if (Input.GetKeyDown (KeyCode.E)) { //Deploy light pylon
+	//Construction Time for a pylon
+	IEnumerator PylonConstructionTime(Vector3 playerPosition) {
+        print(Time.time);
+        yield return new WaitForSeconds(constructionTime);
+
+		GameObject thisObject = Instantiate(Resources.Load("pylon"), playerPosition, Quaternion.identity) as GameObject;
+		thisObject.transform.Translate(transform.forward * 1);
 			
-			//Debug.Log(">>>> Place pylon");
-			if(PlayerGUI.batteryPerc > 15)
-			{
+		SoundManager.instance.PlaySingle(constructionTimeSoundEnd);
 
+        print(Time.time);
+    }
+
+    //Construction Time for a light turret
+    IEnumerator LightTurretConstructionTime(Vector3 playerPosition) {
+        print(Time.time);
+        yield return new WaitForSeconds(constructionTime);
+	
+		GameObject thisObject = Instantiate(Resources.Load("turretPrefab"), playerPosition, Quaternion.identity) as GameObject;
+		thisObject.transform.Translate(transform.forward * 1);
+		
+		SoundManager.instance.PlaySingle(constructionTimeSoundEnd);
+
+        print(Time.time);
+    }
+
+
+	void LateUpdate () {
+
+		if (Input.GetKeyDown (KeyCode.E)) { //Deploy light pylon
+			if(PlayerGUI.batteryPerc > 15) {
 				Vector3 playerPosition = GameObject.Find("Auron").transform.position;
-
-				GameObject thisObject = Instantiate(Resources.Load("pylon"), playerPosition, Quaternion.identity) as GameObject;
-				thisObject.transform.Translate(transform.forward * 1);
 				PlayerGUI.batteryPerc -=15;
+				SoundManager.instance.PlaySingle(constructionTimeSoundBegin);
+				StartCoroutine(PylonConstructionTime(playerPosition));
 			}
-			SoundManager.instance.PlaySingle(buildSound);
 		}
+
+		if (Input.GetKeyDown (KeyCode.R)) { //Deploy light turret
+			if(PlayerGUI.batteryPerc > 10) {
+				Vector3 playerPosition = GameObject.Find("Auron").transform.position;
+				PlayerGUI.batteryPerc-=10;
+				SoundManager.instance.PlaySingle(constructionTimeSoundBegin);
+				StartCoroutine(LightTurretConstructionTime(playerPosition));
+			}
+		}
+
 		if (Input.GetKeyDown (KeyCode.Q)) { //Deploy mine
 			
 			//Debug.Log(">>>> Place mine");
@@ -37,21 +72,6 @@ public class BuildDefenceObject : MonoBehaviour {
 				GameObject thisObject = Instantiate(Resources.Load("lightMine"), playerPosition, Quaternion.identity) as GameObject;
 				thisObject.transform.Translate(transform.forward * 1);
 				PlayerGUI.batteryPerc -=10;
-			}
-			SoundManager.instance.PlaySingle(buildSound);
-		}
-
-		if (Input.GetKeyDown (KeyCode.R)) { //Deploy light turret
-			
-			//Debug.Log(">>>> Place light turret");
-			if(PlayerGUI.batteryPerc > 10)
-			{
-				Vector3 playerPosition = GameObject.Find("Auron").transform.position;
-				
-				GameObject thisObject = Instantiate(Resources.Load("turretPrefab"), playerPosition, Quaternion.identity) as GameObject;
-				thisObject.transform.Translate(transform.forward * 1);
-				PlayerGUI.batteryPerc-=10;
-
 			}
 			SoundManager.instance.PlaySingle(buildSound);
 		}
