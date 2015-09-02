05f4238cc6843128e5eb2be80ab7f216b66663c8
Juan-Paul Naude <ju.naude@gmail.com>
2015-09-02 10:31:12 +0200

    PopUpMenu(Destory option) + MotherF*#@ing Lightning!!!

---
+++ Assets/Scripts/Flicker.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+
+public class Flicker : MonoBehaviour { 
+	public float minIntensity = 0.25f; 
+	public float maxIntensity = 0.5f;
+	public float speed = 0.07f;
+	private float randomizer = 0f;
+	//private GameObject lightT;
+	private Light myLight;
+	
+	void Start()
+	{
+		myLight = GetComponent<Light>();
+	}
+	
+	void FixedUpdate()
+	{
+		randomizer = Random.Range (0f, 1.1f);
+		if (randomizer <= speed) 
+		{
+			//myLight.enabled = false;
+			myLight.intensity = minIntensity;
+		} 
+		else 
+		{
+			//myLight.enabled = true;
+			myLight.intensity = maxIntensity;
+		}
+	}
+
+}
---
+++ Assets/Scripts/HoverMenu.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoverMenu : MonoBehaviour {
+
+	public GUISkin gameSkin;
+	public float menuDistanceFromObj;
+	
+	public string objName;
+	private bool displayObjName = false;
+	GameObject thisObj;
+	private Behaviour h;
+	private bool exitBool;
+	private float posx;
+	private float posy;
+	public float MenuTimer;
+	private float timer;
+	private bool checkPos;
+
+	void Start()
+	{
+		thisObj = this.gameObject;
+	    h = (Behaviour)GetComponent ("Halo");
+		h.enabled = false;
+		checkPos = false;
+		timer = MenuTimer;
+	}
+	void OnGUI()
+	{
+		GUI.skin = gameSkin;
+		displayMenu();
+	}
+
+	void Update()
+	{
+		if (exitBool)
+		{
+			displayObjName = false;
+			h.enabled = false;
+		}
+
+	}
+
+	void OnMouseDown()
+	{
+		h.enabled = true;
+		displayObjName = true; 
+		exitBool = false;
+	}
+
+	public void displayMenu()
+	{
+
+		if (displayObjName) 
+		{ 
+			if(!checkPos)
+			{
+				posx = Event.current.mousePosition.x;
+				posy = Event.current.mousePosition.y;
+				checkPos = true;
+			}
+			MenuTimer -=Time.deltaTime;
+			//Rect rect = new Rect(posx-50, posy-50,200,200);
+			//GUI.Box( new Rect(posx-50, posy-50,200,200),"here");
+			GUI.Box(new Rect(posx+menuDistanceFromObj, posy-10,100,100),objName); //after objName there is another parameter you can add to add a layout skin eg objname,"layoutskin"); the " " is NB also can change settings via the GUI.Skin would recommend that
+			//Inside GuiSkin (Temp one i created in materials) u can add Custom Styles so choose that
+
+			//if(GUI.Button (new Rect (posx+menuDistanceFromObj, posy + 70,70,20), "Exit") || !rect.Contains(Input.mousePosition))// Option 2 : Buggy code that checks if the mouse is still in range of the object
+	      if(GUI.Button (new Rect (posx+menuDistanceFromObj, posy + 70,70,20), "Exit") || MenuTimer <= 0) //Two options  : Option 1 : one that uses a Timer but then the menu is only available for 10 seconds 
+		   {
+				exitBool = true;
+				MenuTimer = timer;
+				checkPos = false;
+			}
+			if(GUI.Button (new Rect (posx+menuDistanceFromObj, posy + 20,70,20), "Upgrade"))
+			{
+
+			}
+			if(GUI.Button (new Rect (posx+menuDistanceFromObj, posy + 40,70,20), "Destroy"))
+			{
+				if(thisObj.tag == "Pylon")
+				{
+				   Destroy(this.gameObject);
+				   PlayerGUI.batteryPerc +=10;
+				}
+				else if(thisObj.tag == "Turret")
+				{
+					Destroy(this.gameObject);
+					PlayerGUI.batteryPerc +=5;
+				}
+
+			}
+		}
+	}
+}
