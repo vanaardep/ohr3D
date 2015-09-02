440c2784f490a898e1f5ccf13ccc8f482cde9b34
Juan-Paul Naude <ju.naude@gmail.com>
2015-09-02 23:20:45 +0200

    Added menu to glowstick+Mine / Menu position
    Added an adjustment to the menu position based on if it
    was at the edges of the screen
    
    *NB NB -  Change New turret to behave like old one + add menu

--- Assets/Scripts/HoverMenu.cs
+++ Assets/Scripts/HoverMenu.cs
@@ -8,7 +8,7 @@ public class HoverMenu : MonoBehaviour {
 	public Texture2D gui_main;
 	public GUIStyle mainFont;
 
-	public float menuDistanceFromObj;
+	//public float menuDistanceFromObj;
 	
 	public string objName;
 	private bool displayObjName = false;
@@ -23,6 +23,7 @@ public class HoverMenu : MonoBehaviour {
 
 	private bool toggleOnOff = true;
 
+
 	void Start()
 	{
 		thisObj = this.gameObject;
@@ -30,6 +31,8 @@ public class HoverMenu : MonoBehaviour {
 		h.enabled = false;
 		checkPos = false;
 		timer = MenuTimer;
+		//Debug.Log ("Screen Width : " + Screen.width);
+		//Debug.Log ("Screen Height : " + Screen.height);
 	}
 	void OnGUI()
 	{
@@ -64,21 +67,34 @@ public class HoverMenu : MonoBehaviour {
 				posx = Event.current.mousePosition.x;
 				posy = Event.current.mousePosition.y;
 				checkPos = true;
+				//Debug.Log ("Position : "+posy);
+				//Debug.Log ("Screen Width : " + Screen.width);
+				//Adjusts the y position of the menu if the user clicks on the asset when the asset is placed on the top/Bottom edges of the screen
+				 if(posy >= Screen.height -100)
+				{
+					//Debug.Log ("Check");
+					posy = posy -100;
+				}
+				else if (posy <= 50)
+				{
+					posy +=20;
+					//Debug.Log ("Check2");
+				}
 			}
 			MenuTimer -=Time.deltaTime;
 			//Rect rect = new Rect(posx-50, posy-50,200,200);//FOR OPTION2
 			//GUI.Box( new Rect(posx-50, posy-50,200,200),"here");//FOR OPTION2
-			//GUI.Box(new Rect(posx+menuDistanceFromObj, posy-10,100,100),objName); //after objName there is another parameter you can add to add a layout skin eg objname,"layoutskin"); the " " is NB also can change settings via the GUI.Skin would recommend that
+			GUI.Box(new Rect(posx+10, posy-10,100,100),objName); //after objName there is another parameter you can add to add a layout skin eg objname,"layoutskin"); the " " is NB also can change settings via the GUI.Skin would recommend that
 			//Inside GuiSkin (Temp one i created in materials) u can add Custom Styles so choose that
 			//if(GUI.Button (new Rect (posx+menuDistanceFromObj, posy + 70,70,20), "Exit") || !rect.Contains(Input.mousePosition))// Option 2 : Buggy code that checks if the mouse is still in range of the object
 	      
 			// Background
-			GUI.DrawTexture (new Rect (posx, posy, 100, 120), gui_main, ScaleMode.StretchToFill, true, 0F);
-
+			//GUI.DrawTexture (new Rect (posx+ 10, posy, 100, 120), gui_main, ScaleMode.StretchToFill, true, 0F);
+			// Took it out for debugging cause u havent created/assigned a skin yet
 
 			if(thisObj.tag == "Pylon")
 			{
-				toggleOnOff = GUI.Toggle(new Rect(posx + 20, posy + 10, 80, 20), toggleOnOff, "On/Off");
+				toggleOnOff = GUI.Toggle(new Rect(posx + 10, posy + 10, 80, 20), toggleOnOff, "On/Off");
 
 				PylonManager script = thisObj.GetComponent<PylonManager>();
 				script.setStatus(toggleOnOff);
@@ -97,11 +113,11 @@ public class HoverMenu : MonoBehaviour {
 				}
 
 			}
-			if(GUI.Button (new Rect (posx + 15, posy + 40,70,20), "Upgrade", mainFont))
+			if(GUI.Button (new Rect (posx + 10, posy + 40,70,20), "Upgrade", mainFont))
 			{
 
 			}
-			if(GUI.Button (new Rect (posx + 15, posy + 65,70,20), "Destroy", mainFont))
+			if(GUI.Button (new Rect (posx + 10, posy + 65,70,20), "Destroy", mainFont))
 			{
 				if(thisObj.tag == "Pylon")
 				{
@@ -113,10 +129,19 @@ public class HoverMenu : MonoBehaviour {
 					Destroy(this.gameObject);
 					PlayerGUI.batteryPerc +=5;
 				}
+				else if(thisObj.tag =="glowStick")
+				{
+					Destroy (this.gameObject);
+					BuildDefenceObject.signalCount++;
+				}
+				else if(thisObj.tag =="Mine")
+				{
+					Destroy(this.gameObject);
+					PlayerGUI.batteryPerc +=5;
+				}
 
 			}
-			Debug.Log ("Menu Timer : " + MenuTimer);
-			if(GUI.Button (new Rect (posx + 15, posy + 90,70,20), "Exit", mainFont) || MenuTimer <= 0) //Two options  : Option 1 : one that uses a Timer but then the menu is only available for 10 seconds 
+			if(GUI.Button (new Rect (posx + 10, posy + 90,70,20), "Exit", mainFont) || MenuTimer <= 0) //Two options  : Option 1 : one that uses a Timer but then the menu is only available for 10 seconds 
 			{
 				exitBool = true;
 				MenuTimer = timer;
