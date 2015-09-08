1bfb824aaa5316e4abb4864027e0cda1e57d50c5
Daniel R Pearson <daniel.pearson@telkomsa.net>
2015-09-08 21:39:58 +0200

    Fixed all known issues of horde waves
    Tell me if you find a bug :')

--- Assets/Scripts/Enemy/EnemyManager.cs
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -2,11 +2,6 @@ using UnityEngine;
 using System.Collections;
 
 public class EnemyManager : MonoBehaviour {
-
-	//Player has to kill 20 enemies in a horde wave
-	//Once done, player has 30 seconds to collect droppables
-	//Horde comes after 30 seconds starting from when the last enemy died from the last horde wave
-
 	public GUIStyle mainFont;
 	public GUIStyle smallFont;
 	public GUIStyle waveFont;
@@ -17,15 +12,16 @@ public class EnemyManager : MonoBehaviour {
 	public float startTimeReset;
 	public Transform[] spawnPoints;
 	public AudioClip hordeSpawnSound;
-	int numberOfEnemies = 0;
+	int numberOfEnemies;
 
 	public float hordeSpawnTime;
 	public int hordeWaveCount;
 	public int maxHordeWaves;
-	public static int enemyKillCount = 0;
+	public static int enemyKillCount;
 	private float timestamp;
 	public int enemiesPerWave;
-	private bool flag1 = false;
+	private bool flag1;
+	private float barTime;
 	public BatterySpawnManager bsm;
 	public LightbulbSpawnManager lsm;
 	
@@ -35,13 +31,17 @@ public class EnemyManager : MonoBehaviour {
 		numberOfEnemies = 0;
 		hordeWaveCount = 0;
 		timestamp = 0.0f;
+		flag1 = false;
+		enemyKillCount = 0;
+		numberOfEnemies = 0;
+		barTime = 0f;
 
 		//edit these to adjust difficulty of the game
-		//spawnrate = 3.0f;
-		//startTime = 30.0f;
-		//startTimeReset = 30.0f; //must be the same as startTime always changed from 30
-		//enemiesPerWave = 20;
-		//maxHordeWaves = 6;
+		spawnrate = 3.0f;
+		startTime = 45.0f;
+		startTimeReset = 45.0f; //must be the same as startTime always changed from 30
+		enemiesPerWave = 30;
+		maxHordeWaves = 6;
 	}
 
 	/**
@@ -53,24 +53,27 @@ public class EnemyManager : MonoBehaviour {
 		Instantiate (enemy, spawnPoints [spawnPointIndex].position, spawnPoints [spawnPointIndex].rotation);
 		numberOfEnemies++;
 	}
+
 	
 	void FixedUpdate () {
 
+		barTime += Time.deltaTime;
 		if(hordeWaveCount != maxHordeWaves) {
+
 
 			//countdown timer of next wave
 			if(hordeSpawnTime >= 0){
-				hordeSpawnTime = Mathf.FloorToInt(startTime - Time.time);
+				hordeSpawnTime = Mathf.FloorToInt(startTime - barTime + 1f);
 			}
 			else {
-				//hordeSpawnTime =  0;
+				//hordeSpawnTime = Mathf.FloorToInt(1f);
 			}
 			
 			//iterates once a wave
 			//sets up wave
-			if ((Time.time >= startTime)  && (numberOfEnemies == 0) && flag1 == false) {
+			if ((barTime >= startTime)  && (numberOfEnemies == 0) && flag1 == false) {
 				enemyKillCount = enemiesPerWave;
-				timestamp = spawnrate + Time.time;	
+				timestamp = spawnrate + barTime;	
 				SoundManager.instance.PlayEnvironmentAudio(hordeSpawnSound);
 				hordeWaveCount++;
 				flag1 = true;
@@ -78,7 +81,7 @@ public class EnemyManager : MonoBehaviour {
 
 			//iterates for every enemy spawned in wave
 			//spawns an enemy
-			if((Time.time >= timestamp)&& (numberOfEnemies != enemiesPerWave) && (flag1 == true)) {	
+			if((barTime >= timestamp)&& (numberOfEnemies != enemiesPerWave) && (flag1 == true)) {	
 				spawn();
 				timestamp += spawnrate;
 					
@@ -86,12 +89,12 @@ public class EnemyManager : MonoBehaviour {
 
 			//iterates after last enemy in a wave has spawned
 			//resets the wave so first if statement will execute
-			if(numberOfEnemies == enemiesPerWave && enemyKillCount == 0) {
+			if(numberOfEnemies == enemiesPerWave) {
 				flag1 = false;
 				numberOfEnemies = 0;
 				hordeSpawnTime = startTimeReset;
 				startTime = startTimeReset;
-				startTime = Time.time + startTime;
+				startTime = barTime + startTime;
 				bsm.batterySpawnLoad();
 				lsm.lightbulbSpawnLoad();
 			}
@@ -99,13 +102,21 @@ public class EnemyManager : MonoBehaviour {
 	}
 
 	void OnGUI(){
-		//GUI.Box (new Rect (0, 200, 190, 50), "Horde wave: " + hordeWaveCount + "/6" );
-		//GUI.Box (new Rect (0, 250, 190, 50), "Next Horde wave spawns in: " + hordeSpawnTime);
-		//GUI.Box (new Rect (0, 300, 200, 50), "Enemies left: " + enemyKillCount + "num: " + numberOfEnemies);
-		//GUI.Box (new Rect (0, 350, 190, 50), "startTime:" + startTime + "TIME: " + Time.time);
+		/*GUI.Box (new Rect (0, 200, 190, 50), "Horde wave: " + hordeWaveCount + "/6" );
+		GUI.Box (new Rect (0, 250, 190, 50), "Next Horde wave spawns in: " + hordeSpawnTime);
+		GUI.Box (new Rect (0, 300, 200, 50), "Enemies left: " + enemyKillCount + "num: " + numberOfEnemies);
+		GUI.Box (new Rect (0, 350, 190, 50), "startTime:" + startTime + "TIME: " + barTime);*/
 
-		GUI.Label (new Rect (Screen.width - 100, 0, 100, 80), "" + hordeSpawnTime, mainFont);
-		GUI.Label (new Rect (Screen.width - 100, 40, 100, 100), "NEXT WAVE", smallFont);
+		
+
+		if(hordeSpawnTime >= 0){
+				GUI.Label (new Rect (Screen.width - 100, 0, 100, 80), "" + hordeSpawnTime, mainFont);
+				GUI.Label (new Rect (Screen.width - 100, 40, 100, 100), "NEXT WAVE", smallFont);
+		}
+		else{
+				GUI.Label (new Rect (Screen.width - 100, 0, 100, 80), "0", mainFont);
+				GUI.Label (new Rect (Screen.width - 100, 40, 100, 100), "Survive!", smallFont);
+		}
 		GUI.Label (new Rect (Screen.width / 2 - 50, 0, 100, 70), "WAVE " + hordeWaveCount + "/6", waveFont);
 	}
 }
