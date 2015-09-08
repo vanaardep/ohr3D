1a0877b98b90f3cdcf9fe276999725bdd2ad521c
vanaardep <pdvanaarde@gmail.com>
2015-09-08 20:55:26 +0200

    Continued tutorial

---
+++ Assets/Scripts/Tutorial/BuildTutorial.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildTutorial : MonoBehaviour {
+
+	public static int signalCount;
+	private int constructionTime;
+	public AudioClip buildSound;
+	public AudioClip constructionTimeSoundBegin;
+	public AudioClip constructionTimeSoundEnd;
+	// Use this for initialization
+	void Start () {
+		signalCount = 4;
+		constructionTime = 3;
+	}
+	
+	//Construction Time for a pylon
+	IEnumerator PylonConstructionTime(Vector3 playerPosition, Vector3 playerRelative, GameObject constructor) {
+		print(Time.time);
+		yield return new WaitForSeconds(constructionTime);
+		Destroy(constructor);
+		GameObject thisObject = Instantiate(Resources.Load("pylon"), playerPosition, Quaternion.identity) as GameObject;
+		
+		thisObject.transform.position = playerPosition + playerRelative * 1;
+		SoundManager.instance.PlayBuildingAudio(constructionTimeSoundEnd);
+		
+		print(Time.time);
+	}
+	
+	//Construction Time for a light turret
+	IEnumerator LightTurretConstructionTime(Vector3 playerPosition, Vector3 playerRelative, GameObject constructor) {
+		print(Time.time);
+		yield return new WaitForSeconds(constructionTime);
+		Destroy(constructor);
+		GameObject thisObject = Instantiate(Resources.Load("turretPrefab"), playerPosition, Quaternion.identity) as GameObject;
+		
+		thisObject.transform.position = playerPosition + playerRelative * 1;
+		SoundManager.instance.PlayBuildingAudio(constructionTimeSoundEnd);
+		
+		print(Time.time);
+	}
+	
+	
+	void LateUpdate () {
+		
+		if (Input.GetKeyDown (KeyCode.E)) { //Deploy light pylon
+		
+				Vector3 playerPosition = GameObject.Find("Auron").transform.position;
+				Vector3 playerRelative = GameObject.Find("Auron").transform.forward;
+				GameObject constructor = Instantiate(Resources.Load("Construction"), playerPosition, Quaternion.identity) as GameObject;
+				
+				constructor.transform.position = playerPosition + playerRelative * 1;
+				
+				PlayerGUI.batteryPerc -=15;
+				SoundManager.instance.PlayBuildingAudio(constructionTimeSoundBegin);
+				
+				StartCoroutine(PylonConstructionTime(playerPosition, playerRelative, constructor));
+		
+		}
+		
+		if (Input.GetKeyDown (KeyCode.R)) { //Deploy light turret
+
+				Vector3 playerPosition = GameObject.Find("Auron").transform.position;
+				Vector3 playerRelative = GameObject.Find("Auron").transform.forward;
+				GameObject constructor = Instantiate(Resources.Load("Construction"), playerPosition, Quaternion.identity) as GameObject;
+				
+				constructor.transform.position = playerPosition + playerRelative * 1;
+				
+				PlayerGUI.batteryPerc -=10;
+				SoundManager.instance.PlayBuildingAudio(constructionTimeSoundBegin);
+				
+				StartCoroutine(LightTurretConstructionTime(playerPosition, playerRelative, constructor));
+	
+		}
+		
+		if (Input.GetKeyDown (KeyCode.Q)) { //Deploy mine
+			
+
+				Vector3 playerPosition = GameObject.Find("Auron").transform.position;
+				
+				GameObject thisObject = Instantiate(Resources.Load("lightMine"), playerPosition, Quaternion.identity) as GameObject;
+				thisObject.transform.Translate(transform.forward * 1);
+				PlayerGUI.batteryPerc -=10;
+
+			SoundManager.instance.PlayBuildingAudio(buildSound);
+		}
+		
+		if (Input.GetKeyDown (KeyCode.G)) { //Deploy glowStick
+			
+			//Debug.Log(">>>> Place glowStick");
+			if(signalCount >=0)
+			{
+				Vector3 playerPosition = GameObject.Find("Auron").transform.position;
+				
+				GameObject thisObject = Instantiate(Resources.Load("glowStick"), playerPosition, transform.rotation) as GameObject;
+				signalCount--;
+			}
+			//thisObject.transform.Translate(transform.forward * 1);  // Disabled so that glow stick is place underneath player, else it is placed next to him.
+			SoundManager.instance.PlayBuildingAudio(buildSound);
+		}
+	}
+}
--- Assets/Scripts/Tutorial/GUITutorial.cs
+++ Assets/Scripts/Tutorial/GUITutorial.cs
@@ -5,48 +5,121 @@ public class GUITutorial : MonoBehaviour {
 
 	public GUIStyle dialogFont;
 	public float dialogTimer;
-	public float dialogTime;
 
 	public string bottomText;
 
+	MovieTexture loading_video;
+	bool loading = false;
+
 	// Player tests
+	float timeElapsed = 0;
 	bool usedTesla = false;
+	bool usedPylon = false;
+	bool usedGlowstick = false;
+	bool usedMine = false;
+	bool usedTurret = false;
 
 	// Use this for initialization
 	void Start () {
-		dialogTime = 4;
+		loading_video = (MovieTexture) Resources.Load( "loading" , typeof( MovieTexture ) );
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		dialogTimer = Time.time;
 
-		// Used tesla gloce
+		// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> SKIP TUT 
+		if (Input.GetKeyUp (KeyCode.Escape)) {
+			endTutorial();
+		}
+
+		// Used tesla glove
 		if (Input.GetKeyUp (KeyCode.F)) {
 			usedTesla = true;
+		}
+
+		// Used pylon
+		if (Input.GetKeyUp (KeyCode.E)) {
+			usedPylon = true;
+		}
+
+		// Used turret
+		if (Input.GetKeyUp (KeyCode.R)) {
+			usedTurret = true;
+		}
+
+		// Used Mine
+		if (Input.GetKeyUp (KeyCode.Q)) {
+			usedMine = true;
+		}
+
+		// Used Glowstick
+		if (Input.GetKeyUp (KeyCode.G)) {
+			usedGlowstick = true;
 		}
 	}
 
 	void OnGUI () {
 
-		Debug.Log (dialogTimer);
+		//Debug.Log (dialogTimer);
 
 		// 1st dialog
-		if (dialogTimer < dialogTime) {
+		if (dialogTimer < 3) {
 			bottomText = "Where am I...";
-		} else if (dialogTimer < dialogTime + 3) {
+		} else if (dialogTimer < 6) {
 			bottomText = "Son, I'm here to explain to you how to survive in this cruel world.";
-		} else if (dialogTimer < dialogTime + 6) {
-			bottomText = "Your first line of defence is your torch, shine it at a creature of darkness to stun them.";
-		} else {
-			bottomText = "Once you are able, press 'F' to use your testla glove to banish the creature.";
+		} else if (dialogTimer < 9) {
+			bottomText = "Your objective is to protect both yourself and your base vehicle. Your first line of defence is your torch, shine it at a creature of darkness to stun them.";
+		} else if (dialogTimer < 12) {
+			bottomText = "To move, use the W A S D keys.";
+		} else if (!usedTesla){
+			bottomText = "Once you are ready, press 'F' to use your testla glove to banish the creature.";
+			timeElapsed = dialogTimer;
 		}
 
 		// Test used tesla
 		if (usedTesla) {
 			bottomText = "Good! This may work for one creature of darkness, but for many you will have to get more creative with your defences.";
+
+			if (dialogTimer - timeElapsed > 6) {
+				bottomText = "For a stronger defence, you can place Light Pylons which ward off creatures of darkness. To place one press 'E'";
+			}
 		}
 
+		// Test used pylon
+		if (usedPylon) {
+			bottomText = "Now, being warned of approaching creatures is important. A Glowstick will warn you of nearby creatures by glowing brighter as they approach. To place one press 'G'.";
+		}
+
+		// Test used glow
+		if (usedGlowstick) {
+			bottomText = "Great! To destroy creatures of darkness before they reach you or your base vehicle, you can place Light Mines. To place one press 'Q'.";
+		}
+
+		// Test used turret
+		if (usedMine) {
+			bottomText = "Good. Your final means of attack is a Light Turret. A Light Turrent will destroy creatures if they enter it's attack radius. To place one press 'R'.";
+		}
+
+		// Test used glow
+		if (usedTurret) {
+			bottomText = "You are now ready to fight off creatures of darkness. Goodluck.";
+			Invoke ("endTutorial", 4);
+		}
+
+		// Draw label
 		GUI.Label (new Rect (30, Screen.height - 70, Screen.width - 60, 70), bottomText, dialogFont);
+		GUI.Label (new Rect (Screen.width - 140, 10, 130, 70), "Press Esc to Skip.", dialogFont);
+
+		if (loading) {
+			GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), loading_video, ScaleMode.ScaleAndCrop, true, 0F);
+			loading_video.Play();
+			loading_video.loop = true;
+		}
+	}
+
+	void endTutorial () {
+		loading = true;
+		Application.LoadLevel ("Level1");
 	}
 }
