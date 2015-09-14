38ae291ddc1343944f5d769b0206edb79b19e0e8
vanaardep <pdvanaarde@gmail.com>
2015-09-14 20:08:11 +0200

    Tweaked player lighting + added base car lights + fog

---
+++ Assets/Scripts/BaseCar/IndicatorFlash.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IndicatorFlash : MonoBehaviour {
+
+	Light lightInd;
+	bool lightOn = true;
+
+	// Use this for initialization
+	void Start () {
+		lightInd = GetComponent<Light>();
+		InvokeRepeating ("toggleLight", 0, 0.5F);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void toggleLight () {
+		Debug.Log ("CALL");
+		if (lightOn) {
+			lightInd.enabled = false;
+			lightOn = false;
+		} else {
+			lightInd.enabled = true;
+			lightOn = true;
+		}
+	}
+}
