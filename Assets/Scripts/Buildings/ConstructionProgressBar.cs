5c0cd819a7084f38f581e61e691cb5c2e66cf3ec
Daniel R Pearson <daniel.pearson@telkomsa.net>
2015-09-17 13:58:59 +0200

    Progress bar added to construction of objects

---
+++ Assets/Scripts/Buildings/ConstructionProgressBar.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ConstructionProgressBar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Image image = GetComponent<Image>();
+		image.fillAmount = Mathf.MoveTowards(image.fillAmount, 1f, Time.deltaTime * .2f);
+	}
+}
