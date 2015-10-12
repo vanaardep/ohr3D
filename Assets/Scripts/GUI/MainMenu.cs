019831131650dd4eff14e81dbd369ab2e05e0589
vanaardep <pdvanaarde@gmail.com>
2015-10-12 16:42:52 +0200

    Added level select

--- Assets/Scripts/GUI/MainMenu.cs
+++ Assets/Scripts/GUI/MainMenu.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class MainMenu : MonoBehaviour {
 
 	public Texture2D menu_background;
+	public Texture2D menu_logo;
+	public Texture2D menu_logo_levels;
 	public Texture2D menu_splash;
 	public AudioClip mainMenuClickSound;
 	MovieTexture loading_video;
@@ -14,12 +16,15 @@ public class MainMenu : MonoBehaviour {
 
 	bool loading = false;
 	bool splash = true;
+	bool menuSelect = false;
 
 	// Use this for initialization
 	void Start () {
 		Invoke ("disableSplash", 4);
-
 		loading_video = (MovieTexture) Resources.Load( "loading" , typeof( MovieTexture ) );
+
+		//TAKE THIS OUT
+		splash = false;
 	}
 	
 	// Update is called once per frame
@@ -35,19 +40,58 @@ public class MainMenu : MonoBehaviour {
 		// Background
 		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), menu_background, ScaleMode.ScaleAndCrop, true, 0F);
 
-		if (GUI.Button (new Rect (Screen.width - 120, Screen.height - 60, 100, 50), "EXIT", mainFont)) {
-			// Exit game
-			SoundManagerMenu.instance.PlayMainMenuClickAudio(mainMenuClickSound);
-			Application.Quit();
-		}
+		// Logo
+		GUI.DrawTexture(new Rect((Screen.width / 2) - 90, (Screen.height / 2) - 200, 180, 300), menu_logo, ScaleMode.StretchToFill, true, 0F);
 
-		if (GUI.Button (new Rect (Screen.width - 120, Screen.height - 120, 100, 50), "START", mainFont)) {
-			// Start game
-			loading = true;
-			SoundManagerMenu.instance.PlayMainMenuClickAudio(mainMenuClickSound);
-			Application.LoadLevelAsync("Tutorial_Objectives");
-		}
+		if (!menuSelect) {
+			if (GUI.Button (new Rect (Screen.width - 120, Screen.height - 60, 100, 50), "EXIT", mainFont)) {
+				// Exit game
+				SoundManagerMenu.instance.PlayMainMenuClickAudio (mainMenuClickSound);
+				Application.Quit ();
+			}
 
+			if (GUI.Button (new Rect (Screen.width - 120, Screen.height - 120, 100, 50), "LEVELS", mainFont)) {
+				// Level Select
+				SoundManagerMenu.instance.PlayMainMenuClickAudio (mainMenuClickSound);
+				menuSelect = true;
+				menu_logo = menu_logo_levels;
+			}
+
+			if (GUI.Button (new Rect (Screen.width - 120, Screen.height - 180, 100, 50), "START", mainFont)) {
+				// Start game
+				loading = true;
+				SoundManagerMenu.instance.PlayMainMenuClickAudio (mainMenuClickSound);
+				Application.LoadLevelAsync ("Tutorial_Objectives");
+			}
+		} else {
+			// Draw click boxes for levels
+			GUI.backgroundColor = Color.clear;
+			if (GUI.Button (new Rect ((Screen.width / 2) - 25, (Screen.height / 2) - 190, 45, 45), "")) {
+				// Tutorial
+				loading = true;
+				SoundManagerMenu.instance.PlayMainMenuClickAudio (mainMenuClickSound);
+				Application.LoadLevelAsync ("Tutorial_Objectives");
+			}
+			if (GUI.Button (new Rect ((Screen.width / 2) - 75, (Screen.height / 2) - 160, 45, 45), "")) {
+				// Level 1
+				loading = true;
+				SoundManagerMenu.instance.PlayMainMenuClickAudio (mainMenuClickSound);
+				Application.LoadLevelAsync ("Level1");
+			}
+			if (GUI.Button (new Rect ((Screen.width / 2) + 30, (Screen.height / 2) - 160, 45, 45), "")) {
+				// Level 2
+				loading = true;
+				SoundManagerMenu.instance.PlayMainMenuClickAudio (mainMenuClickSound);
+				Application.LoadLevelAsync ("Level2");
+			}
+			if (GUI.Button (new Rect ((Screen.width / 2) - 25, (Screen.height / 2) - 70, 45, 45), "")) {
+				// Level 3
+				loading = true;
+				SoundManagerMenu.instance.PlayMainMenuClickAudio (mainMenuClickSound);
+				Application.LoadLevelAsync ("Level3");
+			}
+		}
+		
 		// Splash
 		if (splash) {
 			GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), menu_splash, ScaleMode.ScaleAndCrop, true, 0F);
