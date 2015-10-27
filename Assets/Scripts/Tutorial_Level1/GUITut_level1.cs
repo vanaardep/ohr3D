174a8901cee7f0d6fcd530140852b43bbce19cd0
vanaardep <pdvanaarde@gmail.com>
2015-10-27 18:17:28 +0200

    Added some hints to level 1

--- Assets/Scripts/Tutorial_Level1/GUITut_level1.cs
+++ Assets/Scripts/Tutorial_Level1/GUITut_level1.cs
@@ -30,17 +30,19 @@ public class GUITut_level1 : MonoBehaviour {
 	void OnGUI () {
 
 		// 1st dialog
-		if (dialogTimer < 10 && !pressedSpace) {
-			bottomText = "You have 60 seconds. Scavenge as many batteries and light bulbs as you can.";
+		if (dialogTimer < 20 && !pressedSpace) {
+			bottomText = "HINT: You have 60 seconds. Scavenge as many batteries and light bulbs as you can.";
 			//Time.timeScale = 0;
-		} else if (dialogTimer > 30 && dialogTimer < 35) {
-			bottomText = "You have 30 seconds left, get back to base to start placing defences.";
-		} else if (dialogTimer > 35 && dialogTimer < 40) {
-			bottomText = "Press E to place a Defence Pylon on each of the 3 red spots.";
-		} else if (dialogTimer > 40 && dialogTimer < 45) {
-			bottomText = "Press Q to place Light Mines near your base car.";
-		} else if (dialogTimer > 45 && dialogTimer < 50) {
-			bottomText = "Now use F to shoot your Tesla glove and kill enemies.";
+		} else if (dialogTimer > 30 && dialogTimer < 40) {
+			bottomText = "HINT: You have 30 seconds left, get back to base to start placing defences.";
+		} else if (dialogTimer > 40 && dialogTimer < 50) {
+			bottomText = "HINT: Press E to place a Defence Pylon.";
+		} else if (dialogTimer > 50 && dialogTimer < 60) {
+			bottomText = "HINT: Press Q to place Light Mines near your base car.";
+		} else if (dialogTimer > 60 && dialogTimer < 70) {
+			bottomText = "HINT: Press R to place a Light Turret.";
+		} else if (dialogTimer > 70 && dialogTimer < 80) {
+			bottomText = "HINT: Now use RIGHT CLICK to shoot your Tesla glove and kill enemies.";
 		} else {
 			bottomText = "";
 		}
