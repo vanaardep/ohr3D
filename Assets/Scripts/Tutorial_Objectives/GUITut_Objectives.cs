8c8bd1a841aaf3e5ad02c73f96a0f7603da0b46d
vanaardep <pdvanaarde@gmail.com>
2015-10-28 07:56:54 +0200

    Added skip tut part 2

--- Assets/Scripts/Tutorial_Objectives/GUITut_Objectives.cs
+++ Assets/Scripts/Tutorial_Objectives/GUITut_Objectives.cs
@@ -79,8 +79,8 @@ public class GUITut_Objectives : MonoBehaviour {
 	
 	public void endTutorial () {
 		loading = true;
-		//StopAllAudio (); DO NOT CALL THIS FUNCTION FOR NOW. ONLY WAY TO PLAY BACKGROUND MUSIC
-		Application.LoadLevel ("Tutorial_Video_Title_Part2"); // Make it the next tut screen
+		StopAllAudio ();
+		Application.LoadLevel ("Tutorial_Video_Title_Part2"); 
 	}
 
 	void StopAllAudio() {
--- Assets/Scripts/Tutorial_Objectives/GUITut_Video.cs
+++ Assets/Scripts/Tutorial_Objectives/GUITut_Video.cs
@@ -4,26 +4,42 @@ using System.Collections;
 public class GUITut_Video : MonoBehaviour {
 
 	MovieTexture tutorial_video;
+	public GUIStyle dialogFont;
+
+	private AudioSource[] allAudioSources;
 
 	// Use this for initialization
 	void Start () {
 		tutorial_video = (MovieTexture) Resources.Load( "ohr_tutorial" , typeof( MovieTexture ) );
-		Invoke ("videoDone", 80);
+		Invoke ("endTutorial", 80);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			endTutorial();
+		}
 	}
 
 	void OnGUI () {
 		
-		// Game over video
+		// video
 		tutorial_video.Play ();
 		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), tutorial_video, ScaleMode.ScaleAndCrop, true, 0F);
+
+		// Skip button
+		GUI.Label (new Rect (Screen.width - 150, Screen.height - 80, 140, 80), "Press Space to skip tutorial part 2.", dialogFont);
 	}
 
-	void videoDone () {
-		Application.LoadLevel ("Level1");
+	public void endTutorial () {
+		StopAllAudio ();
+		Application.LoadLevel ("Menu");
+	}
+
+	void StopAllAudio() {
+		allAudioSources = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+		foreach( AudioSource audioS in allAudioSources) {
+			audioS.Stop();
+		}
 	}
 }
